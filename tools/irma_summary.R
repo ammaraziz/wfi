db74a4f6ae04ace56375f005b5f8cc4a431a1318
Ammar Aziz <spidermine63@gmail.com>
2021-03-29 19:48:40 +1100

    R summary script updated - #3

--- tools/irma_summary.R
+++ tools/irma_summary.R
@@ -35,8 +35,9 @@ usage_message = "Failed to parse command-line parameters.
 This script parses IRMA output files in a directory to produce useful plots collated in one location. 
 Usage:
       tmp_script.R -i [INPUTDIR] -o [OUTPUTDIR] -r [FLU or RSV]
-      tmp_script.R -i /analysis/iseq24/ -o /analysis/iseq24/report/ -r flu
+      tmp_script.R -i .../assemblies/ -o .../report/ -r flu
 
+Input directory needs to be the wfi output directory called 'assemblies'
 The -r or --organism flag is needed because IRMA produces a file for each assembled contig (for flu thats 8 genes)
 "
 
@@ -58,58 +59,64 @@ if ( !(toupper(opts$organism) %in% c('RSV', 'FLU')) ) {
   print(opts)
   stop(paste("Organism selected:", opts$organism, 'is not RSV or FLU.'))}
 
-######################vars for plotting
+###################### vars for plotting
 rsv_gene_locs = list(ns1 = 576, ns2 = 1097, n = 2329, p = 3220, m = 4180, sh = 4599, g = 5565, f = 7521, m2 = 8527, l = 15037)
 
-gcolor = list("A_HA_H1" = '#a6cee3', "A_MP" = '#1f78b4', "A_NA_N1" = '#b2df8a', "A_NP" = '#33a02c',
-              "A_NS" = '#fb9a99', "A_PA" = '#ff7f00', "A_PB1" = '#6a3d9a', "A_PB2" = '#b15928')
+gcolor = list("A_HA_H1" = '#a6cee3', 
+              "A_HA_H3" = '#a6cee3',
+              "A_MP" = '#1f78b4', 
+              "A_NA_N1" = '#b2df8a', 
+              "A_NA_N2" = '#b2df8a',
+              "A_NP" = '#33a02c',
+              "A_NS" = '#fb9a99', 
+              "A_PA" = '#ff7f00', 
+              "A_PB1" = '#6a3d9a', 
+              "A_PB2" = '#b15928')
 
 
 ###################################
 
-source("generate_report.R")
+source("./tools/generate_report.R")
 
 main  <- function() {
- 
-  # runs everything
-  #write.csv("this is tmeporary", file = opts$output)
-  
-  base_location = opts$input
+
+  #print params for user
+  write(paste0("organism: ", opts$organism), stdout())
+  write(paste0("input: ", opts$input), stdout())
+  write(paste0("output: ", opts$output), stdout())
+
+  base_location = as.character(opts$input)
 
   locations_aa = get_data_location(base_location, type = 'aa')
   data_aa = get_data(locations_aa)
   data_aa_avg = lapply(data_aa, FUN = average_counts, type = 'aa', by_num = 5)
-  
+
   #RSV
   if (opts$organism == "RSV") {
-    write("rsv", stdout())
-    names(data_aa_avg) = str_extract(locations_aa, pattern = "(^\\w*)")
+    names(data_aa_avg) = str_match(locations_aa, pattern = "(\\w+)/tables")[,2]
     #plot
-    final_out = plot_combine(data_avg = data_aa_avg, 
-                           plot_func = plot_rsv_cov, 
-                           plot_names = names(data_aa_avg))
-    
+    final_out = plot_combine_rsv(data_avg = data_aa_avg, base_location = base_location)
+  
+    #FLU
   } else if (opts$organism == "FLU") {
     # rename
-    file_names = str_split(locations_aa, pattern = "/", simplify = T) %>% 
-      as_tibble() %>% 
-      select(sample = V6, file_name = V8) %>%
+    file_names = tibble(
+      sample = str_match(locations_aa, pattern = "(N100.+?)/tables")[, 2],
+      file_name = str_match(locations_aa, pattern = "[A|B]_\\w{2,3}.+")) %>% 
       mutate(gene = str_replace(file_name, pattern = "-allAlleles.txt", replacement = "")) %>%
-      mutate(sample_gene = paste0(sample, "/", gene)) 
-    names(data_aa_avg) = file_names$sample_gene
+      mutate(sample_gene = paste0(sample, "/", gene))
     
+    names(data_aa_avg) = file_names$sample_gene
     #plot
-    final_out = plot_combine(data_avg = data_aa_avg, 
-                             plot_func = plot_flu_cov, 
-                             plot_names = file_names$sample_gene, 
-                             gene = file_names$gene, 
-                             sample = file_names$sample)
+    final_out = plot_combine_flu(data_avg = data_aa_avg,
+                             gene = file_names$gene,
+                             sample = file_names$sample,
+                             base_location = base_location)
   } else {
     stop("Error: Organism not RSV or FLU. Check input")
   }
-  
-  ggsave(filename = "run_report.pdf", 
-         plot = final_out, device = "pdf", path = opts$output, 
+  ggsave(filename =  opts$output,
+         plot = final_out, device = "pdf",
          dpi = 300,  width = 420, height = 297, units = 'mm')
 }
 
